3c5c7ff7c2e9d2609d95c1ac2645d6fe0f270c0d
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-01-09 22:57:00 +0530

    mysql

--- be.sh
+++ be.sh
@@ -61,6 +61,9 @@ VALIDATE $? "create app "
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "dwnld be "
 
+cd /app
+rm -rf /app/*
+
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzip file"
 
